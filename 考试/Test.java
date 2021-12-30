e37662b67ea1f1fd5bad88233de59ae120f2dccc
lejun <lejun3389@qq.com>
2021-12-31 01:03:15 +0800

    20211230bug

--- 考试/Test.java
+++ 考试/Test.java
@@ -2,8 +2,8 @@ package test.didi.考试;
 
 public class Test {
 //    这是main分支的内容了
-public static void main(String[] args) {
-    System.out.println("bug");
-}
-
+    public static void main(String[] args) {
+        System.out.println("bug");
+        //这是20211230
+    }
 }
